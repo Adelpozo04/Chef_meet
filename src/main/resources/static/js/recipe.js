17908a8729633d8b5817e6654383d2830b3ed4c6
Adelpozo04 <addelp01@ucm.es>
2026-02-12 19:48:35 +0100

    Logica de añadir ingrediente

--- src/main/resources/static/js/recipe.js
+++ src/main/resources/static/js/recipe.js
@@ -1,32 +1,78 @@
 
+
+//Metodo para mostrar una imagen de preview antes de enviar el formulario
 document.addEventListener("DOMContentLoaded", function () {
    
+    //Tomamos los elementos que componen la imagen que son el input, la img de preview y el controlador para subir la imagem
     const input = document.getElementById('imageRecipe');
     const preview = document.getElementById('imageRecipePreview');
     const controller = document.getElementById('imageRecipeController');
 
+    //Tras esto escuchamos el evento change que se da cuando se sube un archivo a la web
     input.addEventListener("change", function (event){
 
+        //Nos guardamos el archivo subido
         const file = event.target.files[0];
 
+        //Si no hay archivo no se hace nada
         if(!file) return;
 
+        //Si el archivo no es una imagen no se hace nada y se avisa del error al usuario
         if(!file.type.startsWith("image/")){
             alert("Selecciona una imagen valida");
             input.value = "";
             return;
         }
 
+        //Nos creamos un lector de archivos
         const reader = new FileReader();
 
+        //Mediante este leemos el archivo antes guardado y lo ponemos como src de la imagen preview, tras lo cual se hace visible
+        //Tambien se esconde el controlador anteriormente usado
         reader.onload = function(e){
             preview.src = e.target.result;
             preview.classList.remove("d-none");
             controller.classList.add("d-none");
         }
 
+        //Convertimos el archivo a una ruta leible por la web.
         reader.readAsDataURL(file);
 
     });
 
 });
+
+document.addEventListener("DOMContentLoaded", function (){
+
+    const button = document.getElementById("addIngredient");
+    const list = document.getElementById("ingredientsList");
+
+    button.addEventListener("click", function() {
+
+        //Creamos un elemento de la lista
+        const newElement = document.createElement("li");
+
+        //Hacemos que dicho elemento pueda ser editable con un texto
+        newElement.contentEditable = true;
+
+        newElement.dataset.placeholder = "Escribe un ingrediente...";
+
+        newElement.classList.add("editable");
+
+        //Impedimos al usuario escribir en varias lineas con Enter
+        newElement.addEventListener("keydown", function(e) {
+            if (e.key === "Enter") {
+                e.preventDefault();
+                newElement.blur();
+            }
+        });
+        
+
+        list.appendChild(newElement);
+        
+        newElement.focus();
+
+    })
+
+})
+
