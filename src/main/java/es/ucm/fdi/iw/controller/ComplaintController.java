43cdd897fbebc083e97a68598b99474e7ef2cf5b
Adelpozo04 <addelp01@ucm.es>
2026-05-13 00:14:53 +0200

    Metidos usuarios, comunidades y eventos en quejas, sin testeo de almacenado

--- src/main/java/es/ucm/fdi/iw/controller/ComplaintController.java
+++ src/main/java/es/ucm/fdi/iw/controller/ComplaintController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.transaction.Transactional;
 import es.ucm.fdi.iw.model.User;
-
+import es.ucm.fdi.iw.model.Community;
 import es.ucm.fdi.iw.model.Complaint;
+import es.ucm.fdi.iw.model.Event;
 import es.ucm.fdi.iw.model.Recipe;
 import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpSession;
@@ -34,9 +35,24 @@ public class ComplaintController {
             "SELECT r FROM Recipe r ORDER BY r.title ASC", Recipe.class)
             .getResultList();
 
-        model.addAttribute("AllRecipes", allRecipes);
+        List<Event> allEvents = entityManager.createQuery(
+            "SELECT e FROM Event e ORDER BY e.title ASC", Event.class)
+            .getResultList();
 
+        List<Community> allCommunities = entityManager.createQuery(
+            "SELECT c FROM Community c ORDER BY c.title ASC", Community.class)
+            .getResultList();
+
+        List<User> allUsers = entityManager.createQuery(
+            "SELECT u FROM User u ORDER BY u.username ASC", User.class)
+            .getResultList();
+
+        model.addAttribute("AllRecipes", allRecipes);
+        model.addAttribute("AllEvents", allEvents);
+        model.addAttribute("AllCommunities", allCommunities);
+        model.addAttribute("AllUsers", allUsers);
         model.addAttribute("complaint", new Complaint());
+        
         return "complaint/create";
     }
 
